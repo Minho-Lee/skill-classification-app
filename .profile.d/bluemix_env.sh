a0fb5874d8a0e88f96a8785dd015f80d75d7efe3
Minho Lee <minho.lee.93@hotmail.com>
2018-05-07 12:39:03 -0400

    removing unnecessary files

--- .profile.d/bluemix_env.sh
+++
@@ -1,7 +0,0 @@
-export MANAGEMENT_SERVER_URL="https://rtmgmt.ng.bluemix.net"
-export cloud_controller_url="https://api.ng.bluemix.net"
-export authorization_endpoint="https://login.ng.bluemix.net/UAALoginServerWAR"
-export BOOT_SCRIPT=app.js
-export NODE_EXECUTABLE=node
-export NODE_OPTS=""
-export NODE_ARGS=""
